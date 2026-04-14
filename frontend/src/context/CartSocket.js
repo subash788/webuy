8c34682f52c373134a8528a32357f7df7aac3f7e
Subash .B <em18subash2122@gmail.com>
2026-04-14 22:15:42 +0530

    Change CartSocket URL to production endpoint

--- frontend/src/context/CartSocket.js
+++ frontend/src/context/CartSocket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const cartSocket = io("http://localhost:4000");
+const cartSocket = io("https://webuy-backend-0459.onrender.com");
 
 export default cartSocket;
