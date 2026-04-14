ced73a6e25733ec6f5dbdf60ebf9f67f65f6c7e9
Subash .B <em18subash2122@gmail.com>
2026-04-14 23:16:02 +0530

    Update Shop.jsx

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
 import Popular from '../components/Popular/Popular';
-import Offers from '../components/Offers/Offers';
+// import Offers from '../components/Offers/Offers'; ❌ remove
 
 const Shop = () => {
   return (
     <div>
       <Hero/>
       <Popular/>
-     {/* <Offers/> */} 
     </div>
   )
 }
