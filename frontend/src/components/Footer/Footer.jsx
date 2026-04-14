fa911c4a06b09dfe8622b16359b6e57e5f134579
Subash .B <em18subash2122@gmail.com>
2026-04-14 23:05:04 +0530

    Add alt text to footer images and update copyright symbol

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -5,14 +5,14 @@ import instagram_icon from '../Assets/instagram_icon.png'
 import pinterest_icon from '../Assets/pintester_icon.png'
 import whatsapp_icon from '../Assets/whatsapp_icon.png'
 
-
 const Footer = () => {
   return (
     <div className='footer'>
         <div className="footer-logo">
-            <img src={footer_logo} alt="" />
+            <img src={footer_logo} alt="WeBuy Logo" />
             <p>WeBuy</p>
         </div>
+
         <ul className="footer-links">
             <li>Company</li>
             <li>Products</li>
@@ -20,21 +20,24 @@ const Footer = () => {
             <li>About</li>
             <li>Contact</li>
         </ul>
+
         <div className="footer-social-icon">
             <div className="footer-icons-container">
-              <img src={instagram_icon}/>
-            </div>
-             <div className="footer-icons-container">
-              <img src={pinterest_icon}/>
-            </div>
-             <div className="footer-icons-container">
-              <img src={whatsapp_icon}/>
+              <img src={instagram_icon} alt="Instagram" />
             </div>
 
+            <div className="footer-icons-container">
+              <img src={pinterest_icon} alt="Pinterest" />
+            </div>
+
+            <div className="footer-icons-container">
+              <img src={whatsapp_icon} alt="WhatsApp" />
+            </div>
         </div>
+
         <div className="footer-copyright">
             <hr />
-            <p>Copyright @ 2025 - All Rights Reserved.</p>
+            <p>Copyright © 2025 - All Rights Reserved.</p>
         </div>
     </div>
   )
